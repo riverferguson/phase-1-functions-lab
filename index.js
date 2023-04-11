df0e1bce8da340477088d7f8a1187ea333116130
River Ferguson <riverferguson@gmail.com>
2023-04-10 20:55:24 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+     
+    return Math.abs(blocks - 42)
+    
+}
+
+function distanceFromHqInFeet(blocks) {
+return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, stop) {
+//return Math.abs(start - stop) * 264 
+return stop > start ? (stop - start) * 264 : (start - stop) * 264
+}
+
+function calculatesFarePrice(start, stop) {
+   const distance = distanceTravelledInFeet(start, stop)
+    if (distance < 400) {
+        return 0
+    }
+    else if (distance < 2000) {
+        return (distance - 400) * .02
+    }
+    else if (distance > 2000 && distance < 2500){
+        return 25
+    }
+    else {
+         return 'cannot travel that far'
+    }
+
+    
+    
+    
+}
